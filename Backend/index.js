6b097935c074acd81bde1d552e4ea3ddc1895231
varad pawar <varadpawarm@gmail.com>
2025-01-16 03:27:39 +0530

    Update index.js cors

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ import { carRoutes } from "./routes/websiteRoutes.js";
 const app = express();
 const PORT = process.env.PORT
 
-app.use(cors());
+app.use(cors({ origin: process.env.CORS_ORIGIN.split(",") }));
 
 
 app.use(morgan("common"));
